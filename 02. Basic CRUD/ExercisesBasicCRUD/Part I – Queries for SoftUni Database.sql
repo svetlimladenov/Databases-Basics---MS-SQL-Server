6a4f74e1e95e7a9a99f9837949cbcd485e4ad40d
Svetli Mladenov <svetlimladenov@gmai.com>
2017-10-11 19:37:31 +0300

    Reordered all folders

--- 02. Basic CRUD/ExercisesBasicCRUD/Part I – Queries for SoftUni Database.sql
+++ 02. Basic CRUD/ExercisesBasicCRUD/Part I – Queries for SoftUni Database.sql
@@ -1,44 +1,70 @@
---SELECT FirstName + '.' + LastName + '@softuni.bg' AS [Full Email Address ] FROM Employees
+--SELECT EmployeeID AS [Employee Id],FirstName,LastName,Salary FROM Employees
+--WHERE Salary > 20000 
+--ORDER BY Salary DESC
 
-SELECT DISTINCT Salary FROM Employees 
-ORDER BY Salary
-GO
-
-SELECT * FROM Employees WHERE JobTitle = 'Sales Representative'
-
-SELECT FirstName, LastName, JobTitle FROM Employees WHERE Salary BETWEEN 20000 AND 30000
-
-GO
-SELECT FirstName + ' ' +  MiddleName +' ' + LastName AS [Full Name] FROM Employees 
-WHERE Salary In (25000, 14000, 12500,23600)
-
-SELECT TOP 5 FirstName , LastName FROM Employees 
-ORDER BY Salary DESC
-
-SELECT FirstName, LastName FROM Employees
-WHERE DepartmentID != 4
-
-SELECT * FROM Employees ORDER BY Salary DESC,FirstName,LastName DESC,MiddleName
-GO
-
-CREATE VIEW V_EmployeesSalaries AS
-SELECT FirstName , LastName,Salary FROM Employees
-GO
+CREATE VIEW v_EmployeesNamesAndDepartments AS
+SELECT d.DepartmentID ,
+	   d.Name AS [Department Name],
+	   e.FirstName + ' ' + e.LastName AS [Manager Name]
+FROM Departments AS d
+JOIN Employees AS e ON  d.ManagerID = e.EmployeeID
 
 GO
-CREATE VIEW V_EmployeeNameJobTitle AS
-SELECT FirstName + ' ' + ISNULL(MiddleName, '') + ' ' + LastName AS [Full Name], JobTitle 
-FROM Employees
+SELECT e.FirstName + ' ' + e.LastName AS[Full Name] , 
+	   e.JobTitle AS [Job Title],
+	   e.Salary
+  FROM Employees As e
+
+
+SELECT * FROM Employees 
+WHERE DepartmentID = (SELECT DepartmentID FROM Departments WHERE Name = 'Marketing')
+
+
+SELECT * FROM Employees
+WHERE NOT (ManagerID = 3 OR ManagerID = 4 )
+
+SELECT * FROM Employees
+WHERE ManagerID NOT IN(3,4)
+
+SELECT * FROM Employees
+WHERE MiddleName IS NULL
+
+
+SELECT * FROM v_EmployeesNamesAndDepartments
 GO
 
-SELECT ISNULL(MiddleName,'KOPELE') FROM Employees WHERE MiddleName IS NULL
+CREATE VIEW v_HighestPeak AS
+SELECT TOP 1 * FROM Peaks 
+ORDER BY Elevation DESC 
+GO
 
-SELECT DISTINCT JobTitle FROM Employees
+SELECT * FROM v_HighestPeak
+
+SELECT * FROM Employees
+
+INSERT INTO Employees(FirstName,MiddleName,LastName,DepartmentID,HireDate,Salary,JobTitle) VALUES
+('Svetlin', 'E', 'Mladenov',1,GETDATE(),12456,'CEO')
+
+SELECT * FROM Employees WHERE FirstName = 'Svetlin'
+
+SELECT *
+INTO EmployeesNamesAndDepartments
+FROM v_EmployeesNamesAndDepartments
+
+SELECT * FROM Departments
+
+UPDATE Employees
+SET Salary *= 1.12
+WHERE DepartmentID IN 
+	(SELECT DepartmentID 
+	FROM Departments 
+	WHERE Name IN ('Engineering', 'Tool Design', 'Marketing' ,'Information Services'))
+
+						
+SELECT [Salary] FROM [Employees] 
+	WHERE DepartmentID IN
+	(SELECT DepartmentID FROM Departments
+	WHERE Name IN ('Engineering', 'Tool Design', 'Marketing' ,'Information Services'))
 
 
-SELECT TOP 10 * FROM Projects
-ORDER BY StartDate , Name
 
-
-SELECT TOP 7 FirstName,LastName,HireDate FROM Employees
-ORDER BY HireDate DESC
